094a383205b38bb11349930bb6109cd0ae3a15f3
dienoone <mosherifofficial@gmail.com>
2024-06-23 02:10:37 +0300

    first commit

--- Hubs/Detection.cs
+++ Hubs/Detection.cs
@@ -14,16 +14,27 @@ public class Detection : Hub
     public override Task OnConnectedAsync()
     {
         
-        _logger.LogDebug($"A user is connection with a connection id ${Context.ConnectionId}");
+        _logger.LogWarning($"A user is connection with a connection id ${Context.ConnectionId}");
         return base.OnConnectedAsync();
+    }
+
+    public async Task SendMessage(string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", message);
     }
 
 
     // update image will be class by the rasperbyi:
-    public async void UpdateFile(IFormFile file)
+    public async Task UpdateFile(string file, string fileContent)
     {
-        var data = file.Name;
-        Console.WriteLine(data);
-        await Clients.All.SendAsync("FileUploaded", "Hello from the server");
+        Console.WriteLine(file);
+        Console.WriteLine(fileContent);
+        await Clients.All.SendAsync("FileUploaded", "ras");
+    }
+
+
+    public async Task UploadFile(IFormFile file)
+    {
+        await Clients.All.SendAsync("FormFile", "ras");
     }
 }
