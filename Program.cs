cc7316ef5bd92b7a47a3e529382a10ad5d796b9f
dienoone <mosherifofficial@gmail.com>
2024-06-22 23:09:06 +0300

    first commit

--- Program.cs
+++ Program.cs
@@ -13,10 +13,10 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("CORSPolicy",
         builder => builder
-        .AllowAnyMethod()
-        .AllowAnyHeader()
-        .AllowCredentials()
-        .SetIsOriginAllowed((hosts) => true));
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowAnyOrigin()
+            .AllowCredentials());
 });
 
 var app = builder.Build();
